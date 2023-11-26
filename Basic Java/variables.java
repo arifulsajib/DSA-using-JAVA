ac723415f22b557fbe068d23688e204cfb68e4c6
arifulsajib <allaboutsajib@gmail.com>
2023-11-26 20:56:50 +0600

    basic

---
+++ Basic Java/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    public static void main(String args[]){
+        int a = 10;
+        String b = "sajib";
+        double c = 14.05;
+        char d = 'b'; //single character
+        
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(a+c);
+    }
+}
